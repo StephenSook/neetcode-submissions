316e0f9f8556db356a307f716559a6b5842f4e09
Stephen Sookra <stephensookra@gmail.com>
2026-08-14 18:59:11 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'string'
+print(type(variable))
+variable = ['hi', "my"]
+print(type(variable))

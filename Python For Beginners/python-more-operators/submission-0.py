47714b4a911a3be83b18dee693618b5d3d74e633
Stephen Sookra <stephensookra@gmail.com>
2026-08-14 19:44:57 -0400

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,17 @@
+a, b, c = 2, 8, 5
+
+proudct_of_a_b = 2 * 8
+
+print(proudct_of_a_b // c)
+
+remainder = proudct_of_a_b % c 
+
+print(remainder)
+
+raising = a ** b 
+
+print(raising)
+
+raising_2 = b ** c
+
+print(raising_2)
